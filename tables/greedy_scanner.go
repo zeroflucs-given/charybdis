a72bc9425b1cd582c96135b1144c9568f127f96a
Steve Gray <steve-gray@users.noreply.github.com>
2022-10-13 13:23:26 +1000

    Add GreedyScanner helper for reading data

---
+++ tables/greedy_scanner.go
@@ -0,0 +1,20 @@
+package tables
+
+import "context"
+
+// GreedyScanner is a helper type that lets us read all records from a table without iterating. It
+// is essentally able have its OnPage function passed as the PageHandlerFn to other functions.
+type GreedyScanner[T any] struct {
+	items []*T
+}
+
+// OnPage is a PageHandlerFn that always keeps requesting more data
+func (g *GreedyScanner[T]) OnPage(ctx context.Context, records []*T, pageState []byte) (bool, error) {
+	g.items = append(g.items, records...)
+	return true, ctx.Err()
+}
+
+// Result of the scan operation
+func (g *GreedyScanner[T]) Result() []*T {
+	return g.items
+}
---
+++ tables/greedy_scanner_test.go
@@ -0,0 +1,40 @@
+package tables_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/zeroflucs-given/charybdis/tables"
+)
+
+// TestScanGreedy tests the greedy scanner
+func TestScanGreedy(t *testing.T) {
+	// Test globals
+	ctx := context.Background()
+	manager, err := tables.NewTableManager[Order](ctx,
+		tables.WithCluster(testClusterConfig),
+		tables.WithKeyspace(TestKeyspace),
+		tables.WithTableSpecification(OrdersTableSpec))
+	require.NoError(t, err, "Should not error starting up")
+
+	// Arrange
+	orders := make([]*Order, 500)
+	for i := 0; i < len(orders); i++ {
+		orders[i] = &Order{
+			OrderID:         fmt.Sprintf("greedy-scan-%d", i),
+			ShippingAddress: fmt.Sprintf("Shipping address for %d", i),
+		}
+	}
+	errBulk := manager.InsertBulk(ctx, orders, 4)
+	require.NoError(t, errBulk, "Should not error inserting")
+
+	// Act
+	var scanner tables.GreedyScanner[Order]
+	errScan := manager.Scan(ctx, scanner.OnPage, tables.WithPaging(10, nil))
+
+	// Assert
+	require.NoError(t, errScan, "Should not error scanning")
+	require.GreaterOrEqual(t, len(scanner.Result()), 250, "Should have at least the number of records we expect")
+}
