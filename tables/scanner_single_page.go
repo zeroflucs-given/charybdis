b15add8a264488bc0a0fc24a593cccacdbcc19a2
Steve Gray <steve-gray@users.noreply.github.com>
2022-11-02 10:29:21 +1000

    Add single-page scanner and rename files to align

---
+++ tables/scanner_single_page.go
@@ -0,0 +1,24 @@
+package tables
+
+import "context"
+
+// SinglePageScanner reads a single page of records and stops iteration.
+type SinglePageScanner[T any] struct {
+	items []*T
+}
+
+// Preallocate ensures the slice exists, pre-allocated to a given size
+func (g *SinglePageScanner[T]) Preallocate(cap int) {
+	g.items = make([]*T, 0, cap)
+}
+
+// OnPage is a PageHandlerFn that always keeps requesting more data
+func (g *SinglePageScanner[T]) OnPage(ctx context.Context, records []*T, pageState []byte) (bool, error) {
+	g.items = append(g.items, records...)
+	return false, ctx.Err()
+}
+
+// Result of the scan operation
+func (g *SinglePageScanner[T]) Result() []*T {
+	return g.items
+}
